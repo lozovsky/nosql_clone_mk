eee1f60319a88892b6c7e09bdf9e4b82acacbbe2
mkassjanski <ankoupl@o2.pl>
2018-04-16 22:39:11 +0200

    tabelka z agregracja

--- zaliczenie/bin/agg1.rb
+++ zaliczenie/bin/agg1.rb
@@ -1,7 +1,26 @@
 #!/usr/bin/ruby
 
 require 'mongo'
+require 'builder'
+require 'optparse'
 require_relative '../lib/connection'
+
+options = {}
+optparse = OptionParser.new do |opt|
+  opt.on('-l', '--limit number') { |o| options[:limit] = o }
+end
+
+begin
+  optparse.parse!
+rescue OptionParser::MissingArgument
+  puts "\nUse -l <numer> to set a limit of showing description.\n"
+end
+
+if options[:limit]
+    limit=options[:limit]
+else
+  limit=10
+end
 
 conn = Connection.new
 db = conn.client.database
@@ -12,9 +31,18 @@ results = alarms.find.aggregate([ {'$group' => { '_id' => '$description',
                                                   }
                                                 },
                                   {'$sort' => {count: -1}},
-                                  {'$limit' => 10}
+                                  {'$limit' => Integer(limit)}
                                                 ])
 
-results.each do |result|
-  puts result
+
+data = results.to_a
+xm = Builder::XmlMarkup.new(:indent => 2)
+xm.table {
+  xm.tr { data[0].keys.each { |key| xm.th(key)}}
+  data.each { |row| xm.tr { row.values.each { |value| xm.td(value)}}}
+}
+
+if File.file?('tabelka1.html')
+  File.truncate('tabelka1.html',0)
 end
+File.write('tabelka1.html',"#{xm}")
